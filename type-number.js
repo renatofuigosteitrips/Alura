d6514c863c937db8aa98418f6b4713d29f880031
renatofuigosteitrips <110949169+renatofuigosteitrips@users.noreply.github.com>
2022-08-09 22:18:32 -0300

    Aulas Iniciais Alura

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica=primeiroNumero/segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFLutuante=3.3;
+const pontoFlutuanteSemZero=.5;
+
+const novaOperacao=primeiroNumero/numeroPontoFLutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number
+
+const alura = "Alura";
+
+console.log(alura*primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringdeNumeros = "34567";
+const citacao = 'O Leo disse "Oi!"';
+const citacao2 = "O Leo disse 'oi!'";
+console.log(citacao2)
+
+// concatenação (+)
+
+const frase = "Meu nome é ";
+const meuNome = "Renato";
+
+console.log(frase+meuNome)
+
+//template string OU template literal
